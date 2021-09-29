c983f52fffdae5f6d7bf5db1727f2c946275aef7
StormStarbrickYT <stormstarbrick@gmail.com>
2021-09-29 12:14:45 -0600

    edited projectdarkstorm.bat
    loading

---
+++ .vscode/PROJECTDARKSTORM/LoadingBar.bat
@@ -0,0 +1,44 @@
+@echo off
+color 02
+Title LOADING
+echo.
+cls
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: ²²²²²²²²²²²²²²²²²²²² 1%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 0²²²²²²²²²²²²²²²²²²² 2%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 0²²²²²²²²²²²²²²²²²²² 3%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 00²²²²²²²²²²²²²²²²²² 10%%
+echo ----------------------------------
+ping -n 1 localhost >nul
+cls
+echo.
+echo.
+echo Loading...
+echo ----------------------------------
+echo Progress: 000²²²²²²²²²²²²²²²²² 15%%
+echo ----------------------------------
+ping -n 1 localhost >nul
